705f02517072f63b67e26dcdf8bd72571ba865ed
Minahil shafique <minahal.shafique@gmail.com>
2021-03-15 21:18:33 -0400

    DOM

---
+++ main.js
@@ -0,0 +1,27 @@
+
+const paragraph = document.querySelector('p')
+paragraph.style.color = 'light blue';
+paragraph.textContent = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
+
+const heading = document.querySelector('h1')
+heading.style.fontSize = '48px';
+
+const list13 = document.querySelector('#item-13')
+list13.style.opacity = '0.5'
+
+const list3 = document.querySelector('#item-3')
+list3.textContent  = 'I say, "Hi!"'
+
+const image = document.querySelector('img')
+image.src = 'http://www.tioxic.com/wp-content/uploads/trex_4.jpg'
+image.style.height = '300px'
+
+const image2 = document.querySelector('#img2')
+image2.style.height = '300px'
+
+const list = document.querySelectorAll('li');
+list[list.length -1].id = "item-16"
+list[list.length -1].class = "item"
+list[list.length -1].textContent = "Won't get fooled again."
+
+
